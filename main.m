8a32dba69b07c7dfff936930184d9050ab80f0a4
Gael Mourouga <mouo@zhaw.ch>
2021-05-03 19:23:44 +0200

    changed some functions

--- main.m
+++ main.m
@@ -1,21 +1,5 @@
 clear all;
 
-%------Physical constants
-R = 8.314; %Ideal gas constant (J/K/mol)
-T = 298.15; %absolute temperature (K)
-F = 96485; %Faraday constant (C/mol)
-
-%-----Known Inputs
-I = 0.4; %charging current (A)
-b = 0.9; %fitted constant for the flow rate
-A_e = 2.236*0.4e-4; %electrode area (m^2)
-A_m = 5e-4; %membrane area (m^2)
-a = 4e-5; %fitted constant for the electrolyte flow rate ( (m/s)^(0.1) )
-Ep0 = 0.62; %thermodynamic potential of TEMPO (V)
-En0 = -0.63; %thermodynamic potential of MV (V)
-kp0 = 4.2e-5; %kinetic constant of TEMPO (m/s)
-kn0 = 3.3e-5; %kinetic constant of MV (m/s)
-
 %-----Distributed unputs
 M = 4; %number of distributed inputs (cell resistance, activity ratios and flow rate)
 
